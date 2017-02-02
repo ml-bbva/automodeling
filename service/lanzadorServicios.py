8abdd59c7c81bbdfb7a48a9ab15644d37266a8a8
Pablo <pablosjv@hotmail.com>
2017-02-02 10:06:40 +0100

    Added argparse and configurable logger

--- service/lanzadorServicios.py
+++ service/lanzadorServicios.py
@@ -10,52 +10,81 @@ import numpy
 import logging
 import os
 import signal
+import argparse
 
 # import argparse or click
 # TO SEE DEBUG AND INFO: --log=
 # https://docs.python.org/3/howto/logging.html
-# TODO: configure flags for logger (argparse?)
+# TODO: Check Error Handling
 # TODO: create docstrings
 
-# LOGGER CONFIG NOTE: Now level is INFO
+# ARGPARSE CONFIG
+
+parser = argparse.ArgumentParser(
+            description='Launch several neural networks' +
+            ' in order to achieve hypermaremetring tunning.')
+parser.add_argument('url_entradas', metavar='url_entradas', type=str,
+                    help='url in which the yaml file is located')
+parser.add_argument('access_key', metavar='access_key', type=str,
+                    help='access_key for rancher')
+parser.add_argument('secret_key', metavar='secret_key', type=str,
+                    help='secret_key for rancher')
+parser.add_argument('-l', '--local', action='store_true',
+                    help='Change the config to launchet it in local')
+parser.add_argument('--info', action='store_const', const=20,
+                    help='set log level INFO')
+parser.add_argument('--debug', action='store_const', const=10,
+                    help='set log level DEBUG')
+
+# GET THE ARGUMENTS
+args = parser.parse_args()
+
+# LOGGER CONFIG
 logger = logging.getLogger('AUTOMODELING')
-logger.setLevel(logging.INFO)
-# create console handler and set level to debug
-ch = logging.StreamHandler()
-ch.setLevel(logging.INFO)
 # create formatter
 formatter = logging.Formatter('%(name)s:%(levelname)s\t%(message)s')
+# create console handler
+ch = logging.StreamHandler()
+
+# Set log level
+if args.info:
+    logger.setLevel(args.info)
+    ch.setLevel(args.info)
+elif args.debug:
+    logger.setLevel(args.debug)
+    ch.setLevel(args.debug)
+else:
+    logger.setLevel(logging.WARN)
+    ch.setLevel(logging.WARN)
+
 # add formatter to ch
 ch.setFormatter(formatter)
 # add ch to logger
 logger.addHandler(ch)
 
-
 # PARÁMETROS GLOBALES DE LA EJECUCIÓN
 namespaces_running = 0
 
-# TODO: Add argparse
 # Lectura de parametros para las url y las keys
-url_entradas = str(sys.argv[1])
-logger.info('url de las entradas:' + url_entradas)
-access_key = str(sys.argv[2])
-logger.info('access key:' + access_key)
-secret_key = str(sys.argv[3])
-logger.info('secret key:' + secret_key)
+url_entradas = args.url_entradas
+logger.debug('url de las entradas:' + url_entradas)
+access_key = args.access_key
+logger.debug('access key:' + access_key)
+secret_key = args.secret_key
+logger.debug('secret key:' + secret_key)
 
 entradas = requests.get(url=url_entradas, verify=False)
 entradas = yaml.load(entradas.text)
 logger.info('Obtenido el fichero de configuracion para los parametros')
-print (entradas)
+logger.debug(entradas)
 
 # Obtenemos parametros time_out y namespaces_limit que son globales para todos los stacks
 time_out = entradas["time_out"]
 namespaces_limit = entradas["limit_namespaces"]
 
 
-
 def main():
-    logger.info('COMIENZA PROCESO DE LANZAMIENTO EXPERIMENTOS')
+    print('COMIENZA PROCESO DE LANZAMIENTO EXPERIMENTOS')
     prepareDirectories()
 
     catalogs = [catalog for catalog in entradas["catalog_services"]][::-1]
@@ -100,6 +129,7 @@ def getConfiguration(configuration):
     r = requests.get(url=url_catalog, auth=auth)
     content_all = r.json()
     logger.info('Obtenido el objeto JSON de la API')
+    logger.debug(content_all)
 
     # Obtención de los ficheros de los servicios que hay que arrancar
     files = content_all['files']
@@ -123,7 +153,7 @@ def configurateKubectl(rancher_url):
     # calculo de la ruta relativa donde se encuentra la carpeta .kube
     filepath = "/root/.kube/config"
     os.system("cp config " + filepath)
-    
+
     # Obtenemos la plantilla para el config
     with open(filepath, 'r') as f:
         text = f.read()
@@ -145,7 +175,7 @@ def getDefinedParams(parametros_yml):
     # Obtiene los parametros para un stack del catalogo
     parametros_nombre = []
     parametros = []
-    logger.info(parametros_yml)
+    logger.debug(parametros_yml)
     # Las distintas formas que se consideran son: parametroNombre->n
     # 1. [valorInicial:valorFinal:Salto] -> Lineal
     # 2. TODO: [valorInicial:valorFinal:Función] -> Otro tipo de funcion
@@ -153,7 +183,7 @@ def getDefinedParams(parametros_yml):
     for parametro in parametros_yml:
         logger.info(parametro)
         parametros_nombre.append(parametro)
-        # Obtiene el parametro HIDDEN_SIZE, que es especial 
+        # Obtiene el parametro HIDDEN_SIZE, que es especial
         if(parametro=='HIDDEN_SIZE'):
             layers = [parametros_yml[parametro]['number_units'] for i in range(parametros_yml[parametro]['number_layers'][0])]
             combinations = []
@@ -276,12 +306,12 @@ def start_service(namespace, serviceFile):
 def startKafka(namespace):
     pid = 0
     with open('./results/'+namespace, 'w') as file_results:
-        kafkaConsumer = Popen(['./exec/kafka-console-consumer'], 
+        kafkaConsumer = Popen(['./exec/kafka-console-consumer'],
             env={"KAFKA_SERVICE":"kafka.default.svc.cluster.local",
                 "TOPIC": namespace+"-metrics",
                 "OFFSET": "oldest",
                 "KAFKA_GROUP":namespace},
-            stdout=file_results, 
+            stdout=file_results,
             shell=True,
             preexec_fn=os.setsid)
         pid = kafkaConsumer.pid
@@ -289,7 +319,7 @@ def startKafka(namespace):
     return pid
 
 
-def rm_namespace(namespace,pid):
+def rm_namespace(namespace, pid):
     # Borra el namespace con el nombre dado y su contenido
     global namespaces_running
     # Mata el proceso kafka
@@ -316,8 +346,6 @@ def getResults(namespace):
 
 def killProcess(pid):
     os.killpg(os.getpgid(pid), signal.SIGTERM)
-    
 
 
 main()
-
