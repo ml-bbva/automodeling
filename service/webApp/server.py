c690a2c9216164f58df9f94e337122571551eb8b
Ignacio <ignacio.iba.mar@gmail.com>
2017-02-10 17:56:54 +0100

    posible solucion

--- service/webApp/server.py
+++ service/webApp/server.py
@@ -6,11 +6,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def getResults():
-	#with open('./../launcherApp/results/global_results.json', 'r') as f:
 	with open('/usr/src/myapp/results/global_results.json', 'r') as f:
 		results = json.load(f)
 	return json2html.json2html.convert(json=results)
 
 
 if __name__ == '__main__':
-	app.run(port=5000)
+	app.run(host='0.0.0.0', port=5000)
