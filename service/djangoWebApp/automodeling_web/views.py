6dacb3c3cbb0fe3a6136cf450947d6148611254b
Pablo <pablosjv@hotmail.com>
2017-02-14 09:48:25 +0100

    App to show results added

--- service/djangoWebApp/automodeling_web/views.py
+++ service/djangoWebApp/automodeling_web/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import json2html
+import json
+import os
 
 # Create your views here.
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+def results(request):
+    with open(BASE_DIR + '/results/global_results.json', 'r') as f:
+        results = json.load(f)
+    return HttpResponse(json2html.json2html.convert(json=results))
--- service/djangoWebApp/djangoWebApp/urls.py
+++ service/djangoWebApp/djangoWebApp/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from automodeling_web import views as automodeling_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^automodeling/', automodeling_views.results),
 ]
